42a32fe8d852f1686ff81a7ab60585eadcf52db1
foufouadi <attar.foued@gmail.com>
2026-09-01 17:28:07 +0200

    Use global invocation IDs for compute image coordinates

--- src/SharpEmu.ShaderCompiler.Vulkan/Gen5SpirvTranslator.cs
+++ src/SharpEmu.ShaderCompiler.Vulkan/Gen5SpirvTranslator.cs
@@ -1590,8 +1590,6 @@ public static partial class Gen5SpirvTranslator
                         _uintType,
                         localId,
                         component);
-                    StoreV(component, localComponent, guardWithExec: false);
-
                     var groupComponent = _module.AddInstruction(
                         SpirvOp.CompositeExtract,
                         _uintType,
@@ -1610,6 +1608,7 @@ public static partial class Gen5SpirvTranslator
                             groupComponent,
                             UInt(localSize)),
                         localComponent);
+                    StoreV(component, globalComponent, guardWithExec: false);
                     var limitPointer = _module.AddInstruction(
                         SpirvOp.AccessChain,
                         _pushConstantUintPointer,
