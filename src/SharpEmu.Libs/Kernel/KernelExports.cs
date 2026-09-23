7bc7b6ad615af6c5c3ec4eccde87f50d25acd2bb
foufouadi <attar.foued@gmail.com>
2026-09-23 16:49:35 +0200

    Revert "port kernel debug exception exports"
    This reverts commit 71d605e82c82a4a921862bfc865a104305cf746d.

--- src/SharpEmu.Libs/Kernel/KernelExports.cs
+++ src/SharpEmu.Libs/Kernel/KernelExports.cs
@@ -560,5 +560,16 @@ public static class KernelExports
         return (int)OrbisGen2Result.ORBIS_GEN2_OK;
     }
 
+    #pragma warning disable SHEM004
+    [SysAbiExport(
+        Nid = "cfwBSQyr5Ys",
+        ExportName = "sceKernelInternalUnknownCfwBSQyr5Ys",
+        Target = Generation.Gen4 | Generation.Gen5,
+        LibraryName = "libKernel")]
+    public static int KernelInternalUnknownCfwBSQyr5Ys(CpuContext ctx)
+    {
+        ctx[CpuRegister.Rax] = 0;
+        return (int)OrbisGen2Result.ORBIS_GEN2_OK;
+    }
     #pragma warning restore SHEM004
 }
